e4022c6fdeac7d4ba4a133547c7696b48bd1e752
apetrazz-ops <a.petrazz@gmail.com>
2025-09-02 12:41:20 +0200

    Update vector.h

--- include/vector.h
+++ include/vector.h
@@ -1,1 +1,18 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+#include <stddef.h>
+
+typedef struct {
+    int *data;
+    size_t size;
+    size_t capacity;
+} IntVector;
+
+void vec_init(IntVector *v);
+void vec_free(IntVector *v);
+void vec_push(IntVector *v, int value);
+int  vec_pop(IntVector *v, int *ok);
+double vec_mean(const IntVector *v);
+
+#endif
 
